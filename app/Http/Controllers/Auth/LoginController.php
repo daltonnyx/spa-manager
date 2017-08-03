223e19fd966280b652387a13c5f190c1c780f07b
quytruong851@gmail.com <quytruong851@gmail.com>
2017-08-03 09:48:51 +0700

    Add login and logout for Rest API

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+use Illuminate\Http\Request;
+
 class LoginController extends Controller
 {
     /*
@@ -35,5 +37,30 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request) {
+        $this->validateLogin($request);
+        if( $this->attemptLogin($request) ) {
+            $user = $this->guard()->user();
+            $user->generateToken();
+            return response()->json([
+                'data' => $user->toArray()
+            ]);
+        }
+        return $this->sendFailedLoginResponse($request);
+    }
+
+    public function logout(Request $request) {
+        $user = Auth::guard('api')->user();
+        if($user) {
+            $user->api_token = null;
+            $user->save();
+        }
+    }
+
+    public function sendFailedLoginResponse($request) {
+        $errors = ['error' => trans('auth.failed')];
+        return response()->json($errors, 422);
     }
 }
