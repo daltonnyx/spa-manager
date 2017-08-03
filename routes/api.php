8ec111fbe7614e0ca1fad2263bae67826a178fc0
quytruong851@gmail.com <quytruong851@gmail.com>
2017-08-03 09:52:13 +0700

    Add api route with auth:api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,31 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', 'Auth\LoginController@login');
+Route::post('/logout', 'Auth\LoginController@logout');
+Route::group(['middleware' => 'auth:api'],function() {
+    Route::get('/shops', 'SpaShopController@index');
+    Route::get('/shops/{shop}', 'SpaShopController@show');
+    Route::post('/shops', 'SpaShopController@store');
+    Route::put('/shops/{shop}', 'SpaShopController@update');
+    Route::delete('/shops/{shop}', 'SpaShopController@destroy');
+
+    
+});
+
+Route::group(['middleware' => 'auth:api'], function() {
+    Route::get('/rooms/{shop_id}', 'SpaRoomController@index');
+    Route::get('/rooms/{room}', 'SpaRoomController@show');
+    Route::post('/rooms', 'SpaRoomController@store');
+    Route::put('/rooms/{room}', 'SpaRoomController@update');
+    Route::delete('/rooms/{room}', 'SpaRoomController@destroy');
+});
+
+Route::group(['middleware' => 'auth:api'], function() {
+    Route::get('/bookings/{room_id}', 'SpaBookingController@index');
+    Route::get('/bookings/{booking}', 'SpaBookingController@show');
+    Route::post('/bookings', 'SpaBookingController@store');
+    Route::put('/bookings/{booking}', 'SpaBookingController@update');
+    Route::delete('/bookings/{booking}', 'SpaBookingController@destroy');
+});
