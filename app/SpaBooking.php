f16f7b78c425ae35ed1ed1f7c7cc68f3d8a1bf3c
quytruong851@gmail.com <quytruong851@gmail.com>
2017-08-05 16:42:44 +0700

    set timestamps to false

--- app/SpaBooking.php
+++ app/SpaBooking.php
@@ -10,4 +10,5 @@ class SpaBooking extends Model
     protected $dateFormat = 'd-m-Y H:i';
     protected $table = 'spa_bookings';
     protected $fillable = ['customer_name', 'customer_phone', 'start_on', 'end_on', 'room_id'];
+    public $timestamps = false;
 }
