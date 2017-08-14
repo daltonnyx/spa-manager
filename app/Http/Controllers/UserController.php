c51aa3e092e5814fb3d68ff7261c1362b252b402
Quy Truong <aquez@thinkpadx200.aquez.io>
2017-08-14 10:59:45 +0700

    add them ten va filter user voi role la admin

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        return User::all(['id','email','name']);
+        return User::where('role','admin')->select('id','email','name')->get();
     }
 
     /**
